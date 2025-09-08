8ee2bf126172bcf84a4a7d51126a99bb70c3f6d2
gwirn <71886945+gwirn@users.noreply.github.com>
2025-09-08 09:42:57 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,3 +2,17 @@ import subprocess
 
 # this is a comment
 myproc = subprocess.Popen('/bin/echo', shell=True)
+"""
+this
+is a
+very 
+long 
+comment
+"""
+
+userPwd = "mysupersecretPhrase"
+ 
+password = "noonewilleverguess"
+
+APIKEY =  "AIzaSyDaGmWKa4JsXZ-HjGw7ISLn_3namBGewQe"
+
